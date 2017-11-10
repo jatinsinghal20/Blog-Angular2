9706757acd345b92237b7ee036b6d718b597984b
Jatin Singhal <jatinsinghal20@gmail.com>
2017-11-10 12:08:01 +0530

    welcome to blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './welcome/login/login.component';
+import { NavbarComponent } from './welcome/navbar/navbar.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    LoginComponent,
+    NavbarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
